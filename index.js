88f7bedcb8e1969de7309f5ce3b5b74efb4a2655
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2025-09-07 12:42:08 +0600

    update your card section

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const treeCatagoryContiner = document.getElementById("treecatagorycontiner")
 
 const treeContainer= document.getElementById("treecontainer")
 
+const treecountcontiner= document.getElementById("treecountcontiner")
+
+let treecart=[]
+
 const loadCategory = () =>{    
 fetch('https://openapi.programming-hero.com/api/categories')
  .then(res => res.json())
@@ -17,6 +21,7 @@ fetch('https://openapi.programming-hero.com/api/categories')
     console.log(err)
  })
 }
+
 
 
 
@@ -89,12 +94,13 @@ const treeShowCatagory =(categories)=>{
     }
 
     const showtreesBycategory = (articles)=>{
+        
          treeContainer.innerHTML=""
         // console.log(articles)
         articles.forEach(article =>{
             treeContainer.innerHTML+=`
 
-            <div class="card bg-base-100 shadow-xl p-2">
+            <div id=${article.id} class="card bg-base-100 shadow-xl p-2">
       <img src="${article.image}" alt="" class=" h-[185px] rounded-xl">
       <h2 class="font-bold text-[#1F2937] my-1">${article.name}</h2>
       <p class="mt-2 text-[#1F293770] text-[11px]">${article.description}</p>
@@ -102,7 +108,7 @@ const treeShowCatagory =(categories)=>{
         <button class=" bg-[#15803D80] text-[#15803D] text-[11px] rounded-lg p-1">${article.category}</button>
         <h3 class="text-[#1F2937] font-bold ">$${article.price}</h3>
       </div>
-      <button class="btn bg-[#15803D] rounded-xl text-white w-full">Add to Cart</button>
+      <button id="addtocard" class="btn bg-[#15803D] rounded-xl text-white w-full">Add to Cart</button>
     </div>
 
             `
@@ -113,7 +119,47 @@ const treeShowCatagory =(categories)=>{
 
 
 
+ treeContainer.addEventListener("click",(e)=>{
+    // console.log(e.target.innerText)
+    if(e.target.innerText ==="Add to Cart"){
+        alert("tree has been added to the card")
+        handeltreecart(e)
+    }
+ })
 
+ const handeltreecart =(e)=>{
+
+    // console.log("btn click")
+        const title = e.target.parentNode.children[1].innerText
+        const id =e.target.parentNode.id
+        // console.log(id)
+
+       treecart.push({
+         title: title,
+         id: id
+       })
+         console.log( treecart)
+
+         showtreecart(treecart)
+
+ }
+
+ const showtreecart =(treecart)=>{
+    console.log(treecart)
+     treecountcontiner.innerHTML=""
+     treecart.forEach(item =>{
+        treecountcontiner.innerHTML +=`
+         <div class=" shadow-sm my-3 p-2 ">
+       <div class="flex justify-between">
+        <h1 class="text-[11px] font-bold">${item.title}</h1>
+         <button> ❌</button>
+         </div>
+        </div>
+        
+        `
+     })
+
+ }
 
 
 loadCategory()
