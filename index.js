5d474efef56119078a135102c993c9dfd66ba6e2
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2025-09-07 06:58:09 +0600

    added indecator on Choose Your Trees  section

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const loadCategory = () =>{
 fetch('https://openapi.programming-hero.com/api/categories')
  .then(res => res.json())
  .then(data =>{
-    console.log(data.categories)
+    // console.log(data.categories)
 
     const categories = data.categories
     treeShowCatagory(categories); 
@@ -22,7 +22,8 @@ const treeShowCatagory =(categories)=>{
 
     categories.forEach(cata => {
 
-        console.log(cata.category_name);
+        // console.log(cata.category_name);
+
            treeCatagoryContiner.innerHTML +=`
            
            <li id="${cata.id}" class= " cursor-pointer hover:text-white hover:bg-[#15803D]">${cata.category_name}</li>
@@ -31,6 +32,19 @@ const treeShowCatagory =(categories)=>{
     });
 
 }
+
+    treeCatagoryContiner.addEventListener("click", (e)=>{
+
+        const allli =document.querySelectorAll("li")
+        allli.forEach(li=>{
+            li.classList.remove("bg-[#15803D]")
+        })
+        // console.log(e.target.localName)
+       if(e.target.localName === "li"){
+        // console.log(e.target)
+        e.target.classList.add("bg-[#15803D]")
+       }
+    })
 
 
 
@@ -42,9 +56,3 @@ loadCategory()
 
 
 
-// categories": [
-// {
-// "id": 1,
-// "category_name": "Fruit Tree",
-// "small_description": "Trees that bear edible fruits like mango, guava, and jackfruit."
-// },
