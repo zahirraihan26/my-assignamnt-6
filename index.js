3b3a0d216e60357b7de1f9987b85719ec9d59d8c
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2025-09-07 08:28:54 +0600

    added tree Cards in use api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const treeCatagoryContiner = document.getElementById("treecatagorycontiner")
 
+const treeContainer= document.getElementById("treecontainer")
+
 const loadCategory = () =>{    
 fetch('https://openapi.programming-hero.com/api/categories')
  .then(res => res.json())
@@ -41,11 +43,52 @@ const treeShowCatagory =(categories)=>{
         })
         // console.log(e.target.localName)
        if(e.target.localName === "li"){
-        // console.log(e.target)
+        // console.log(e.target.id)
         e.target.classList.add("bg-[#15803D]")
+        treenewsByCategory(e.target.id)
        }
     })
 
+
+    const treenewsByCategory = (treeId) =>{
+        console.log(treeId)
+        fetch(`https://openapi.programming-hero.com/api/category/${treeId}`)
+         .then(res =>res.json())
+         .then(data => {
+
+            // console.log(data.plants)
+
+             showtreesBycategory(data.plants) 
+
+        })
+
+         .catch(err => {
+            console.log(err)
+     })
+    }
+
+    const showtreesBycategory = (articles)=>{
+         treeContainer.innerHTML=""
+        // console.log(articles)
+        articles.forEach(article =>{
+            treeContainer.innerHTML+=`
+
+            <div class="card bg-base-100 shadow-xl p-6">
+      <img src="${article.image}" alt="" class=" rounded-xl">
+      <h2 class="font-bold text-[#1F2937]">${article.name}</h2>
+      <p class="mt-2 text-[#1F293770]">${article.description}</p>
+      <div class="py-3 flex justify-between items-center">
+        <button class="btn bg-[#15803D80] text-[#15803D] rounded-lg p-1">${article.category}</button>
+        <h3 class="text-[#1F2937] font-bold ">$${article.price}</h3>
+      </div>
+      <button class="btn bg-[#15803D] rounded-xl text-white w-full">Add to Cart</button>
+    </div>
+
+            `
+
+        })
+
+    }
 
 
 
